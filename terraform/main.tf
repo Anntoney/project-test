a98430ff63fba606d2f2425ba81dcb86e36b46e9
Antoney <anjuguna95@gmail.com>
2025-08-11 12:57:45 +0300

    Remove duplicate output definitions from main.tf to fix Terraform validation errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,17 +106,4 @@ resource "google_cloud_run_service_iam_member" "no_public_access" {
   member   = "allUsers"
 }
 
-output "service_url" {
-  value       = google_cloud_run_service.insight_agent.status[0].url
-  description = "The URL of the deployed Cloud Run service"
-}
-
-output "artifact_registry_repository" {
-  value       = google_artifact_registry_repository.insight_agent_repo.name
-  description = "The name of the Artifact Registry repository"
-}
-
-output "service_account_email" {
-  value       = google_service_account.insight_agent_sa.email
-  description = "The email of the service account used by Cloud Run"
-} 
+ 
