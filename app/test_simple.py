bcd2b717d8819ac5b379c702d51f2988059fc26e
Antoney <anjuguna95@gmail.com>
2025-08-11 12:05:19 +0300

    Debug CI/CD: Add simple tests and improve workflow robustness

---
+++ app/test_simple.py
@@ -0,0 +1,8 @@
+# Simple test to debug CI/CD issues
+def test_simple():
+    """Simple test that should always pass"""
+    assert 1 + 1 == 2
+
+def test_string():
+    """Another simple test"""
+    assert "hello" in "hello world" 
